fb3a90adc09ef000b0fc92bf8ea80c55a8f98e07
Michael Schwartz <mykesx@gmail.com>
2018-08-15 16:09:48 -0700

    .clang-format file

---
+++ main/wifi_credentials.sample.h
@@ -0,0 +1,10 @@
+/*
+ * This file allows us to set up WIFI SSD and password not in the repo.
+ * Copy this file to main/wifi_credentials.h and fill in the values.
+ * The wifi_credentials.h is in .gitignore so it won't end up in the repo.
+ */
+
+#ifndef WIFI_SSD
+#define WIFI_SSID "Your SSID"
+#define WIFI_PASS "Your WiFi Password"
+#endif
